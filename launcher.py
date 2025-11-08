56b9436f74ef991353f7ce116eb0336f8ccab525
Austin Henlotter <ahenlotter@umass.edu>
2025-11-08 17:06:36 -0500

    fixed stuff

--- launcher.py
+++ launcher.py
@@ -1,54 +1,80 @@
 #!/bin/python
 import argparse
-import os
+import subprocess
+import threading
 
 import uvicorn
-
 from utils import get_port_no
 
 WRAPPER_MTP = "a,3222;b,3223"
 
+def launch_load():
+    """Launch the load server in a blocking call (to be run in a thread)."""
+    uvicorn.run(
+            "llm-server.server:app",
+            host="0.0.0.0",
+            port=get_port_no("load"),
+            reload=False, 
+            )
+
+def launch_tmb():
+    """Launch the tmb server in a blocking call (to be run in a thread)."""
+    uvicorn.run(
+            "tmb-server.tmb:tmb",
+            host="0.0.0.0",
+            port=get_port_no("tmb"),
+            reload=False,
+            )
+
+def launch_wrapper():
+    """Launch the wrapper executable as a subprocess."""
+    subprocess.Popen([
+        "./wrapper/target/debug/wrapper",
+        WRAPPER_MTP,
+        str(get_port_no("wrapper")),
+        ])
+
+def launch_model():
+    """Launch the model server as a subprocess."""
+    subprocess.Popen([
+        "./llama.cpp/build/bin/llama-server",
+        "-hf", "ggml-org/gemma-3-1b-it-GGUF",
+        "--port", str(get_port_no("model")),
+        ])
+
+def launch_all():
+    load_thread = threading.Thread(target=launch_load, daemon=True)
+    tmb_thread = threading.Thread(target=launch_tmb, daemon=True)
+
+    load_thread.start()
+    print("launched load")
+
+    tmb_thread.start()
+    print("launched tmb")
+
+    launch_wrapper()
+    print("launched wrapper")
+
+    launch_model()
+    print("launched model")
+
+    load_thread.join()
+    tmb_thread.join()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--server", type=str, required=True)
+    parser.add_argument("--server", type=str, required=False, default="all")
     args = parser.parse_args()
+
     match args.server:
         case "load":
-            uvicorn.run(
-                "llm-server.server:app",
-                host="0.0.0.0",
-                port=get_port_no("load"),
-                reload=True,
-            )
-
+            launch_load()
         case "tmb":
-            uvicorn.run(
-                "tmb-server.tmb:tmb",
-                host="0.0.0.0",
-                port=get_port_no("tmb"),
-                reload=True,
-            )
-
+            launch_tmb()
         case "wrapper":
-            os.execv(
-                "./wrapper/target/debug/wrapper",
-                [
-                    "./wrapper/targets/debug/wrapper",
-                    WRAPPER_MTP,
-                    str(get_port_no("wrapper")),
-                ],
-            )
-        case "model":  # if the binary doesnt exist go to llama.cpp, run cmake -B build -DGGML_CUDA=ON -DBUILD_SHARED_LIBS=OFF, cmake --build build --config Release -j 8 --target llama-server
-            executable = "llama.cpp/build/bin/llama-server"
-            os.execv(
-                executable,
-                [
-                    executable,
-                    "-hf",
-                    "ggml-org/gemma-3-1b-it-GGUF",  # model type
-                    "--port",
-                    str(get_port_no("model")),
-                ],
-            )
+            launch_wrapper()
+        case "model":
+            launch_model()
         case _:
-            print("not a valid program name")
+            launch_all()
+
