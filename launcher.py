937f99b448b1755128c17695629daa1ead8729f4
notrodes <rodesmnr@proton.me>
2025-11-08 14:57:17 -0500

    add ml model server llama.cpp

--- launcher.py
+++ launcher.py
@@ -1,7 +1,10 @@
-from utils import get_port_no 
+#!/bin/python
 import argparse
 import os
+
 import uvicorn
+
+from utils import get_port_no
 
 WRAPPER_MTP = "a,3222;b,3223"
 
@@ -11,13 +14,37 @@ if __name__ == "__main__":
     args = parser.parse_args()
     match args.server:
         case "load":
-            uvicorn.run("llm-server.server:app", host="0.0.0.0", port=get_port_no("load"), reload=True)
+            uvicorn.run(
+                "llm-server.server:app",
+                host="0.0.0.0",
+                port=get_port_no("load"),
+                reload=True,
+            )
 
         case "tmb":
-            uvicorn.run("tmb-server.tmb:tmb", host="0.0.0.0", port=get_port_no("tmb"), reload=True)
+            uvicorn.run(
+                "tmb-server.tmb:tmb",
+                host="0.0.0.0",
+                port=get_port_no("tmb"),
+                reload=True,
+            )
 
         case "wrapper":
-            os.execv("./wrapper/wrapper", ["./wrapper/wrapper", WRAPPER_MTP, str(get_port_no("wrapper"))])
+            os.execv(
+                "./wrapper/wrapper",
+                ["./wrapper/wrapper", WRAPPER_MTP, str(get_port_no("wrapper"))],
+            )
+        case "model":  # if the binary doesnt exist go to llama.cpp, run cmake -B build -DBUILD_SHARED_LIBS=OFF, cmake --build build --config Release -j 8 --target
+            executable = "llama.cpp/build/bin/llama-server"
+            os.execv(
+                executable,
+                [
+                    executable,
+                    "-hf",
+                    "ggml-org/gemma-3-1b-it-GGUF",  # model type
+                    "--port",
+                    str(get_port_no("wrapper")),
+                ],
+            )
         case _:
             print("not a valid program name")
-
