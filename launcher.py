823ce003a19874f871e631d84ac6da5db03a718c
Austin Henlotter <ahenlotter@umass.edu>
2025-11-08 10:36:29 -0500

    added wrapper to launcer

--- launcher.py
+++ launcher.py
@@ -1,6 +1,9 @@
 from utils import get_port_no 
 import argparse
+import os
 import uvicorn
+
+WRAPPER_MTP = "a,3222;b,3223"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,6 +15,9 @@ if __name__ == "__main__":
 
         case "tmb":
             uvicorn.run("tmb-server.tmb:tmb", host="0.0.0.0", port=get_port_no("tmb"), reload=True)
+
+        case "wrapper":
+            os.execv("./wrapper/wrapper", ["./wrapper/wrapper", WRAPPER_MTP, str(get_port_no("wrapper"))])
         case _:
             print("not a valid program name")
 
